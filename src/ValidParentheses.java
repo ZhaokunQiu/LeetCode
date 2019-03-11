82779311ab4051b759a34a8c7d6b05b2eafaa282
Qzkkkk <15850691878@163.com>
2019-03-11 20:49:29 +0800

    写了一半

---
+++ src/ValidParentheses.java
@@ -0,0 +1,15 @@
+import java.util.Queue;
+import java.util.Stack;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+        Queue<Integer> temp = new SynchronousQueue<>();
+    }
+
+    public boolean isValid(String s) {
+        return fal;
+    }
+}
