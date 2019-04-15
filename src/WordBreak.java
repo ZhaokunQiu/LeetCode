ee88a5992229d6ceeeb6f7ab427a198b490157c7
Qzkkkk <15850691878@163.com>
2019-04-15 22:26:46 +0800

    判断给定字符串可否由给定子序列组成并给出各个子序列数组

--- src/WordBreak.java
+++ src/WordBreak.java
@@ -9,7 +9,7 @@ public class WordBreak {
         word.add("leet");
         word.add("code");
         String s = "leetcode";
-        w.wordBreak(s,word);
+        w.wordBreak2(s,word);
     }
 
     public boolean wordBreak(String s, List<String> wordDict) {
@@ -33,6 +33,50 @@ public class WordBreak {
         return dp[dp.length - 1];
     }
 
+    List<String> result = new ArrayList<>();
+    public List<String> wordBreak2(String s, List<String> wordDict) {
+        if(s.length() == 0)
+            return result;
+        if(wordDict.size() == 0)
+            return result;
+        boolean dp[] = new boolean[s.length() + 1];
+        dp[0] = true;
+        List<Integer>[] temp = new ArrayList[dp.length];
+        temp[0] = new ArrayList<>();
+        for(int i = 1;i < dp.length;i++){
+            temp[i]  = new ArrayList<>();
+            dp[i] = false;
+            for(int j  = 0;j < wordDict.size();j++){
+                String ss = wordDict.get(j);
+                if(i - ss.length() < 0)
+                    continue;
+                if(dp[i - ss.length()]  && s.substring(i - ss.length(),i).equals(ss)){
+                    dp[i] = true;
+                    temp[i].add(j);
+                }
+            }
+        }
+        int index = dp.length;
+        if(dp[index - 1])
+        generate(index - 1,new LinkedList<>(),wordDict,temp);
+        return result;
+    }
 
+
+    private void generate(int index,  List<Integer> list, List<String> word, List<Integer>[] temp){
+        if(index == 0){
+            StringBuilder sb = new StringBuilder();
+            for(Integer i : list){
+                sb.append(word.get(i) + " ");
+            }
+            result.add(sb.toString().trim());
+            return;
+        }
+        for(Integer i : temp[index]){
+            LinkedList<Integer> nl = new LinkedList<>(list);
+            nl.addFirst(i);
+            generate(index - word.get(i).length(),nl,word,temp);
+        }
+    }
 
 }
