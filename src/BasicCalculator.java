316e7516ac67036cc518f870fbc594ce20c08bd1
Qzkkkk <15850691878@163.com>
2019-07-05 20:16:04 +0800

    计算器未写完

---
+++ src/BasicCalculator.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+
+public class BasicCalculator {
+    public int calculate(String s) {
+        LinkedList<Character> sym = new LinkedList<>();
+        LinkedList<Integer> num = new LinkedList<>();
+        char[] temp = s.toCharArray();
+        for(char tt : temp){
+            switch(tt){
+                case '+': sym.push('+');break;
+                case '-': sym.push('-');break;
+                case '*':
+            }
+        }
+    }
+}
